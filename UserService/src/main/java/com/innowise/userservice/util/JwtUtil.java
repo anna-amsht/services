de1e0799326c65fa423679760fbd9a7aae3f4086
Anna <annamsht13@gmail.com>
2025-11-11 09:51:19 +0300

    added jwt util for userservice

---
+++ UserService/src/main/java/com/innowise/userservice/util/JwtUtil.java
@@ -0,0 +1,97 @@
+package com.innowise.userservice.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class JwtUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+
+    private final SecretKey key;
+    private final JwtParser jwtParser;
+    private final String issuer;
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String jwtSecret,
+            @Value("${jwt.issuer}") String issuer) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        this.jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+        this.issuer = issuer;
+    }
+
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header: {}", bearerToken);
+        
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String getRefreshJwtFromCookies(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("refreshToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return jwtParser.parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public UUID getUserIdFromJwtToken(String token) {
+        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+        Integer userId = claims.get("userId", Integer.class);
+        return UUID.fromString(userId.toString());
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getRoles(String token) {
+        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+        return claims.get("roles", List.class);
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            logger.debug("Validating JWT token");
+            Jws<Claims> claims = jwtParser.parseClaimsJws(authToken);
+            
+            if (!issuer.equals(claims.getBody().getIssuer())) {
+                logger.error("Invalid JWT issuer: expected '{}', but got '{}'", issuer, claims.getBody().getIssuer());
+                return false;
+            }
+            
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        }
+        return false;
+    }
+}
