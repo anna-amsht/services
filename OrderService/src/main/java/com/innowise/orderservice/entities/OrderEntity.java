ac5920240344461d33a1b59073775d0466b80432
Anna <annamsht13@gmail.com>
2025-11-19 04:00:43 +0300

    init project and added changelogs, entities

---
+++ OrderService/src/main/java/com/innowise/orderservice/entities/OrderEntity.java
@@ -0,0 +1,44 @@
+package com.innowise.orderservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime creationDate;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private List<OrderItemEntity> orderItems = new ArrayList<>();
+}
+
