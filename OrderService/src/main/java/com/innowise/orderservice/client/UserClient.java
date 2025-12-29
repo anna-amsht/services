69ccfea35efbc84725560b1e23763f6a7c952837
Anna <annamsht13@gmail.com>
2025-12-29 10:02:49 +0300

    modified controller layer orderservice

--- OrderService/src/main/java/com/innowise/orderservice/client/UserClient.java
+++ OrderService/src/main/java/com/innowise/orderservice/client/UserClient.java
@@ -4,10 +4,11 @@ import com.innowise.orderservice.dto.models.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserClient {
 
-    @GetMapping("/api/v1/users/{id}")
-    UserDto getUserById(@PathVariable("id") Long id);
+    @GetMapping("/api/v1/users/internal/{id}")
+    UserDto getUserById(@PathVariable("id") Long id, @RequestHeader("X-Internal-Token") String internalToken);
 }
