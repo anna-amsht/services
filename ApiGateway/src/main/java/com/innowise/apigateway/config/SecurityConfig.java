754931316d491a769d1611736391cd23712e59c2
Anna <annamsht13@gmail.com>
2026-01-19 12:28:22 +0300

    deployment of apigateway

--- ApiGateway/src/main/java/com/innowise/apigateway/config/SecurityConfig.java
+++ ApiGateway/src/main/java/com/innowise/apigateway/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/api/v1/register", "/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/refresh").permitAll()
+                        .pathMatchers("/actuator/health","/actuator/health/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(jwtAuthFilter, SecurityWebFiltersOrder.HTTP_BASIC)
